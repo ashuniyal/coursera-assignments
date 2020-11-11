25c231555028c27479af846382780d801731a6ea
ashish <ashish@uniyal.com>
2020-11-11 20:45:37 +0530

    updated

--- mod4-solution/components/categories.component.js
+++ mod4-solution/components/categories.component.js
@@ -2,7 +2,7 @@
 
 angular.module('MenuApp')
 .component('categoriesComponent', {
-	templateUrl: "../template/categories.html",
+	templateUrl: "./template/categories.html",
 	bindings: {
 		menus: '<'
 	}
